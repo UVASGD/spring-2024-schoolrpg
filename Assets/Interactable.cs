4953ea19a7ec9eb2192d8d2c1054b82c68cd2f94
Catherine Xu <phf5kw@virginia.edu>
2024-02-10 18:27:48 -0500

    Project Folders

---
+++ Assets/Interactable.cs
@@ -0,0 +1,53 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+
+public class Interactable : MonoBehaviour
+{
+    bool inside = false;
+    public UnityEvent interactAction;
+    private Canvas dialogueCanvas;
+    private bool inDialogue; // make this a STATE?
+
+    // Start is called before first update
+    void Start()
+    {
+        dialogueCanvas = GameObject.Find("DialogueCanvas").GetComponent<Canvas>();
+    }
+
+    void OnTriggerEnter2D(Collider2D col)
+    {
+        if(col.gameObject.tag == "Player")
+        {
+            inside = true;
+        }
+    }
+
+    void OnTriggerExit2D(Collider2D col)
+    {
+        if(col.gameObject.tag == "Player")
+        {
+            inside = false;
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        // If inside interaction zone and dialogue has NOT STARTED, start dialogue.
+
+        if (inside == true && Input.GetKeyDown(KeyCode.E)) 
+        {         
+            if (!dialogueCanvas.enabled)
+            {
+                Debug.Log("START DIALOGUE");
+                interactAction.Invoke();
+            }
+        }
+         
+        // the update has no way of knowing if the dialogue needs to end
+    }
+
+
+}
