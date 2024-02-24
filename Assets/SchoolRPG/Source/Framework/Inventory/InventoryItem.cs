51a0ceea006a4a2e63ce6f8377237362f322ef1e
Aarfdark <aadarsh.natarajan@gmail.com>
2024-02-24 14:37:21 -0500

    setup stuff for inventory

---
+++ Assets/SchoolRPG/Source/Framework/Inventory/InventoryItem.cs
@@ -0,0 +1,26 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[serializable]
+public class InventoryItem
+{
+    public InventoryItemData data { get; private set; }
+    public int stackSize { get; private set; }
+
+    public InventoryItem(InventoryItemData source)
+    {
+        data = source; 
+        addToStack();
+    }
+
+    public void AddToStack()
+    {
+        stackSize++;
+    }
+
+    public void RemoveFromStack()
+    {
+        stackSize--;
+    }
+}
