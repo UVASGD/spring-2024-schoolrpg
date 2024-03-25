e8bb82c18a90cf26dbd1711259785d722a73e678
lillihrncir <hrncirlilli@gmail.com>
2024-03-25 18:39:50 -0400

    Base Puzzle System
    Made a simple toolbar and drag and drop system. Ways to exit and enter the "puzzle screen" are not yet set.

---
+++ Assets/Puzzle_System Scripts/PuzzleScreen.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PuzzleScreen : MonoBehaviour
+{
+    boolean isActive = PuzzleScreenGroup.isActive();
+    boolean frozen = false;
+    // Start is called before the first frame update
+// void Start()
+// {
+    
+// }
+
+// // Update is called once per frame
+// void Update()
+// {
+//     while (isActive)
+// }
+}
