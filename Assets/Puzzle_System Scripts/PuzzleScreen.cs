c24a5eaae840d535cda88c1063a4e180e64be7d2
lillihrncir <hrncirlilli@gmail.com>
2024-04-02 08:40:16 -0400

    Removed Compiler Errors

--- Assets/Puzzle_System Scripts/PuzzleScreen.cs
+++ Assets/Puzzle_System Scripts/PuzzleScreen.cs
@@ -2,19 +2,19 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class PuzzleScreen : MonoBehaviour
-{
-    boolean isActive = PuzzleScreenGroup.isActive();
-    boolean frozen = false;
-    // Start is called before the first frame update
-// void Start()
+// public class PuzzleScreen : MonoBehaviour
 // {
+//     boolean isActive = PuzzleScreenGroup.isActive();
+//     boolean frozen = false;
+//     // Start is called before the first frame update
+// // void Start()
+// // {
     
-// }
+// // }
 
-// // Update is called once per frame
-// void Update()
-// {
-//     while (isActive)
+// // // Update is called once per frame
+// // void Update()
+// // {
+// //     while (isActive)
+// // }
 // }
-}
