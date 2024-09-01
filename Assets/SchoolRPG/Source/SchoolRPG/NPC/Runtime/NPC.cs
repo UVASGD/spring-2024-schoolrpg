34a3a3c63ea55680ee7497e945f3cde07218b0eb
Liam Tolbert <treblotmail@gmail.com>
2024-09-01 19:34:11 -0400

    reinstated npc scripts
    really weird...all the changes before the merge in npc.runtime were gone, so I put them back

---
+++ Assets/SchoolRPG/Source/SchoolRPG/NPC/Runtime/NPC.cs
@@ -0,0 +1,60 @@
+using System.Collections;
+using System.Collections.Generic;
+using SchoolRPG.Inventory.Runtime;
+using UnityEngine;
+
+namespace SchoolRPG.NPC.Runtime
+{
+    [System.Serializable]
+    [CreateAssetMenu(fileName = nameof(NPC), menuName = "ScriptableObjects/NPC/NPC")]
+    public class NPC : ScriptableObject
+    {
+        /// <summary>
+        /// The name that represents this NPC. Also used as an id. 
+        /// </summary>
+        [field: SerializeField]
+        public string Name { get; set; }
+
+        /// <summary>
+        /// The item that the NPC is assigned to and will accept from the player for the player to pass. 
+        /// </summary>
+        [field: SerializeField]
+        public InventoryItem RequiredItem { get; set; }
+
+        /// <summary>
+        /// The level that the NPC will show up in. 
+        /// </summary>
+        [field: SerializeField]
+        public int Level { get; set; }
+
+        /// <summary>
+        /// Whether the NPC has or has not accepted its assigned item from the player.  
+        /// </summary>
+        [field: SerializeField]
+        public bool IsPassed { get; set; }
+
+        /// <summary>
+        /// Whether the NPC has or has not accepted its assigned item from the player.  
+        /// </summary>
+        [field: SerializeField]
+        public string[] Dialogue;
+
+        /// <summary>
+        /// Whether the NPC has or has not accepted its assigned item from the player.  
+        /// </summary>
+        [field: SerializeField]
+        public string[] PassDialogue;
+
+        /// <summary>
+        /// Whether the NPC has or has not accepted its assigned item from the player.  
+        /// </summary>
+        [field: SerializeField]
+        public string[] AlreadyPassedDialogue;
+
+        /// <summary>
+        /// Whether the NPC has or has not accepted its assigned item from the player.  
+        /// </summary>
+        [field: SerializeField]
+        public string[] NoPassDialogue;
+    }
+}
--- Assets/SchoolRPG/Source/SchoolRPG/NPC/Runtime/NpcItemBehavior.cs
+++ Assets/SchoolRPG/Source/SchoolRPG/NPC/Runtime/NpcItemBehavior.cs
@@ -1,6 +1,7 @@
 using SchoolRPG.Dialogue.Runtime;
 using SchoolRPG.Input.Runtime;
 using SchoolRPG.Inventory.Runtime;
+using SchoolRPG.NPC.Runtime;
 using SchoolRPG.ProgressTracker.Runtime;
 using System;
 using System.Collections;
@@ -10,12 +11,14 @@ using UnityEngine;
 public class NpcItemBehavior : MonoBehaviour
 {
     public Inventory inventory;
-    public InventoryItem requiredItem;
-    public ProgressTracker tracker; // Some sort of icon to indicate the NPC has accepted the player's items
+    public ProgressTracker passIndicator;
     [SerializeField]
     private InventoryEventChannel InventoryEventChannel;
     [SerializeField]
     private InventoryManager InventoryManager;
+
+    [SerializeField]
+    private NPC npc;
 
     [SerializeField]
     private DialogueEventChannel dialogueEventChannel;
@@ -24,58 +27,63 @@ public class NpcItemBehavior : MonoBehaviour
     private InputEventChannel inputEventChannel;
 
     private InventoryItem selectedItem;
-    private bool passed;
-    public string[] passDialogue;
     private bool isInventoryOpened;
+
+    private InventoryItem dummyItem;
 
     private void Start()
     {
+        npc.IsPassed = false;
+        dummyItem = ScriptableObject.CreateInstance<InventoryItem>();
         Debug.Log("NPC item behavior activated");
-        passed = false;
     }
 
-    private void OnEnable(){
+    private void OnEnable()
+    {
         inputEventChannel.OnInventory += setIsInventoryOpened;
     }
 
-    private void OnDisable(){
+    private void OnDisable()
+    {
         inputEventChannel.OnInventory -= setIsInventoryOpened;
     }
 
-    private void setIsInventoryOpened(){
-        isInventoryOpened = !isInventoryOpened;
+    private void Update()
+    {
+        Debug.Log(InventoryManager.GetSelectedInventoryItem());
     }
 
-    private void Update(){
-        if (!isInventoryOpened){
-            InventoryEventChannel.RaiseOnSetSelectedInventoryItem(new InventoryItem());
+    private void setIsInventoryOpened()
+    {
+        isInventoryOpened = !isInventoryOpened;
+        if (!isInventoryOpened)
+        {
+            InventoryEventChannel.RaiseOnSetSelectedInventoryItem(dummyItem);
         }
     }
 
     private void OnTriggerStay2D(Collider2D other)
     {
         selectedItem = InventoryManager.GetSelectedInventoryItem();
-        
-        if (other.CompareTag("Player"))
+        if (other.CompareTag("Player") && selectedItem != null && !selectedItem.Equals(dummyItem) && !npc.IsPassed)
         {
-            Debug.Log(selectedItem);
-            if (selectedItem != null && selectedItem.Equals(requiredItem) && !passed)
+            if (selectedItem.Equals(npc.RequiredItem))
             {
                 //placeholder thing for npc allowing player to pass
-                passed = true;
+                npc.IsPassed = true;
                 // need to auto close inventory
                 inputEventChannel.RaiseOnInventory();
-                dialogueEventChannel.RaiseOnOpenDialogueRequested(passDialogue);
+                inventory.inventoryItems.Remove(npc.RequiredItem);
+                dialogueEventChannel.RaiseOnOpenDialogueRequested(npc.PassDialogue);
                 // For some reason, an empty dialogue box appears 
                 inputEventChannel.RaiseOnInteract();
-                selectedItem = null;
-                Debug.Log("Player has the required items for " + gameObject.name);
             }
             else
             {
-                Debug.Log("Player does not have the required items for " + gameObject.name);
+                inputEventChannel.RaiseOnInventory();
+                dialogueEventChannel.RaiseOnOpenDialogueRequested(npc.NoPassDialogue);
+                inputEventChannel.RaiseOnInteract();
             }
         }
     }
-
 }
